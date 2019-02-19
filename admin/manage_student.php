081e86034cdaa1f308f434307d1ea8d7bf31506a
mirasrobert <mrmirasrobert@gmail.com>
2019-02-19 22:46:52 +0800

    I made a file manage_student.php and design.css

---
+++ admin/manage_student.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet"  href="../css/design.css">
+	<link rel="icon" href="../img/lspu.png">
+</head>
+<body>
+		<div class="row">
+			<div class="col-md-2 col-lg-2 ">
+				<div class="csidebar">
+					<br><br>
+
+				</div>
+			</div>
+			<div class="col-md-10 col-lg-10">
+				<nav>
+					<div class="nav-center">
+  				<span><img src="../img/lspu.png" height="100" width="100" /></span>
+  				<span><div class="left-float"><h1>Laguna State Polytechnic University</h1>
+  				<h2>Siniloan (Host) Campus</h2></div></span>
+  				</div>
+  				</nav>
+
+			</div>
+		</div>
+		</div>
+        <div class="back-css">
+          <button type="submit" name="back" >back</button>
+        </div>
+
+          <div class="manage-btn">
+            <button type="submit" name="student-admission" >Student<br>Admission</button>
+            <button  type="submit" name="student-account" style="margin-bottom: 30px;margin-left:50px;">Student<br>Account</button>
+            <button type="submit" name="manage-student-request" style="margin-left: 140px;">Manage Student Request</button>
+          </div>
+</body>
+</html>
+<script src="../js/bootstrap.js">
+
+</script>
