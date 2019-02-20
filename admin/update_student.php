75255150e5a20a436e5ffda85c8b6f00be04035a
mirasrobert <mrmirasrobert@gmail.com>
2019-02-20 20:01:56 +0800

    I made a file update_student.php and modified design.css

---
+++ admin/update_student.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet"  href="../css/design.css">
+	<link rel="icon" href="../img/lspu.png">
+</head>
+<body>
+		<div class="row">
+			<div class="col-md-2 col-lg-2 ">
+				<div class="csidebar">
+					<br><br>
+
+				</div>
+			</div>
+			<div class="col-md-10 col-lg-10">
+				<nav>
+					<div class="nav-center">
+  				<span><img src="../img/lspu.png" height="100" width="100" /></span>
+  				<span><div class="left-float"><h1>Laguna State Polytechnic University</h1>
+  				<h2>Siniloan (Host) Campus</h2></div></span>
+  				</div>
+  				</nav>
+
+			</div>
+		</div>
+		</div>
+    <!-- EDIT PHOTO AVATAR -->
+    <div class="avatar">
+      <img src="../img/default.png" alt="your-photo">
+    </div>
+    <!-- EDIT PHOTO BUTTON -->
+    <div class="photo-edit">
+        <a href="#">Edit photo</a>
+    </div>
+    <p class="std-name">Student Name</p>
+    <div class="back-css">
+      <a href="index.php" class="btn btn-success">Home</a>
+    </div>
+    <!-- Update input  -->
+      <div class="upd-student">
+        <form  action="" method="post" >
+          <label>Name: </label>
+          <input type="text" name="name"><br>
+          <label>Username: </label>
+          <input type="text" name="usn" value=""><br>
+          <label>Current Password: </label>
+          <input type="text" name="current-pwd" value=""><br>
+          <label>New Password: </label>
+          <input type="text" name="new-pwd" value=""><br>
+          <label>Confirm New Password: </label>
+          <input type="text" name="confirm-new-pwd" value=""><br>
+        </div>
+        <div class="upd-student-2">
+          <label>Mobile: </label>
+          <input type="text" name="confirm-new-pwd" value=""><br>
+          <label>Email: </label>
+          <input type="text" name="confirm-new-pwd" value=""><br>
+        </div>
+        <!-- update-btn  -->
+        <div class="upd-btn">
+          <button type="submit" name="upd-btn" class="btn btn-success">Update</button>
+        </div>
+        </form>
+</body>
+</html>
+<script src="../js/bootstrap.js">
+
+</script>
