7d7038586c267e5294c58021f45cd2253b4bdc8e
ramil25 <ramilkaharian25@gmail.com>
2019-02-21 13:36:22 +0800

    I add student table on database please reimport the database the file is inside db folder

--- db/databank.sql
+++ db/databank.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 17, 2019 at 01:10 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.10
+-- Generation Time: Feb 21, 2019 at 06:33 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `databank`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` varchar(30) NOT NULL,
+  `full_name` varchar(30) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `school_graduated` varchar(100) NOT NULL,
+  `strand_course` varchar(50) NOT NULL,
+  `grade_GWA` float NOT NULL,
+  `grade_Math` float NOT NULL,
+  `grade_English` float NOT NULL,
+  `grade_Science` float NOT NULL,
+  `fchoice` varchar(30) NOT NULL,
+  `schoice` varchar(30) NOT NULL,
+  `tchoice` varchar(30) NOT NULL,
+  `raw_score` float NOT NULL,
+  `remarks` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -46,6 +69,13 @@ INSERT INTO `users` (`user_id`, `user_name`, `user_password`, `user_level`) VALU
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`),
+  ADD UNIQUE KEY `full_name` (`full_name`);
 
 --
 -- Indexes for table `users`
