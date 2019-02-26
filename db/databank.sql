133998a8c421ce0dda3608f0058be7ba1246b61e
ramil25 <ramilkaharian25@gmail.com>
2019-02-27 06:28:15 +0800

    modified databank.sql on db folder please reimport your database

--- db/databank.sql
+++ db/databank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2019 at 06:33 AM
+-- Generation Time: Feb 26, 2019 at 11:25 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -32,7 +32,7 @@ CREATE TABLE `students` (
   `student_id` varchar(30) NOT NULL,
   `full_name` varchar(30) NOT NULL,
   `gender` varchar(10) NOT NULL,
-  `school_graduated` varchar(100) NOT NULL,
+  `school_last_attended` varchar(100) NOT NULL,
   `strand_course` varchar(50) NOT NULL,
   `grade_GWA` float NOT NULL,
   `grade_Math` float NOT NULL,
@@ -42,8 +42,17 @@ CREATE TABLE `students` (
   `schoice` varchar(30) NOT NULL,
   `tchoice` varchar(30) NOT NULL,
   `raw_score` float NOT NULL,
-  `remarks` varchar(30) NOT NULL
+  `remarks` varchar(30) NOT NULL,
+  `date_ad` varchar(10) NOT NULL,
+  `photo_link` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `full_name`, `gender`, `school_last_attended`, `strand_course`, `grade_GWA`, `grade_Math`, `grade_English`, `grade_Science`, `fchoice`, `schoice`, `tchoice`, `raw_score`, `remarks`, `date_ad`, `photo_link`) VALUES
+('00000001', 'Dem Soriano', 'Male', 'Laguna Sta Polytechnic University', 'ICT', 89, 88, 75, 80, 'BSIT', 'BSCS', 'BA', 88.9, 'Past', '02/22/2019', '');
 
 -- --------------------------------------------------------
 
